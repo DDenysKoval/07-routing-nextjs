be36cb2ed5fe361e1c0245c4856c894eb8e22b89
Denys Koval <metalheadden@gmail.com>
2025-08-11 12:56:37 +0300

    Initial commit

---
+++ app/notes/page.tsx
@@ -0,0 +1,13 @@
+// import css from "./App.module.css"
+import { fetchNotes } from "@/lib/api"
+import NotesClient from "./Notes.client"
+
+export default async function Notes() {
+  const initialData = await fetchNotes("", 1);
+
+  return (
+    <main>
+      <NotesClient initialData={initialData}/>
+    </main>
+  )
+}
